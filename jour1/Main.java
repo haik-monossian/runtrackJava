da0519b3a8f01dd297e5f539fbd380a21404874e
Hykoo13 <haikmonossiandev@gmail.com>
2026-03-30 14:02:09 +0200

    Job1

---
+++ jour1/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        char c = 'b';
+        String s = "hello";
+        int i = 10;
+        float f = 10.10f;
+        boolean b = true;
+
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(b);
+
+        // int TOTO = 3.817; // .\jour1\Main.java:17: error: incompatible types:
+        // possible lossy conversion
+        // from double to int int TOTO = 3.817; 1 error error: compilation failed
+        // System.out.println(TOTO);
+    }
+}
