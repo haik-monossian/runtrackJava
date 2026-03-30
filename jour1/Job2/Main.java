5e6675f62e9970042ee6772170b1f7fb73bf1f0a
Hykoo13 <haikmonossiandev@gmail.com>
2026-03-30 14:42:41 +0200

    job2

---
+++ jour1/Job2/Main.java
@@ -0,0 +1,17 @@
+package Job2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez votre nom : ");
+
+        String name = scanner.nextLine();
+
+        System.out.println(name);
+
+        scanner.close();
+    }
+}
