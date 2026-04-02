64297e4f052191e1c3f8c76afac98aa04b938d4e
Hykoo13 <haikmonossiandev@gmail.com>
2026-04-02 14:25:28 +0200

    Job1

---
+++ jour4/Job1/MyThread.java
@@ -0,0 +1,7 @@
+package jour4.Job1;
+
+public class MyThread extends Thread {
+    public void run() {
+        System.out.println("thread en cours");
+    }
+}
---
+++ jour4/Job1/ThreadProject.java
@@ -0,0 +1,9 @@
+package jour4.Job1;
+
+public class ThreadProject {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+
+        thread.start();
+    }
+}
